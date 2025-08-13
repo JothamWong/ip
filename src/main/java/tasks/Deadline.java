12bef897d64d26ac3361d9cf2e251e10a2525434
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-08-13 23:58:32 +0800

    feat: Add Deadline class

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,5 +1,7 @@
 package tasks;
 
+import misc.PepeException;
+
 public class Deadline extends Task {
 
     private final String by;
@@ -7,6 +9,36 @@ public class Deadline extends Task {
     public Deadline(String name, String by) {
         super(name);
         this.by = by;
+    }
+
+    public static Deadline fromInput(String[] inputs) throws PepeException {
+        int index = 0;
+
+        StringBuilder name = new StringBuilder();
+        for (; index < inputs.length; index++) {
+            String input = inputs[index];
+            if (input.equals("/by")) {
+                index++; // advance past /by
+                break;
+            } else {
+                name.append(input);
+            }
+        }
+
+        if (name.isEmpty()) {
+            throw new PepeException("Deadline's body expression is empty.");
+        }
+
+        StringBuilder by = new StringBuilder();
+        for (; index < inputs.length; index++) {
+            by.append(inputs[index]);
+        }
+
+        if (by.isEmpty()) {
+            throw new PepeException("Deadline not specified.");
+        }
+
+        return new Deadline(name.toString(), by.toString());
     }
 
     @Override
