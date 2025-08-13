c62309ed0e607ed0dba4928cf330d96fad4dccda
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-08-13 17:57:51 +0800

    feat: Add deadline class

---
+++ src/main/java/tasks/Deadline.java
@@ -0,0 +1,16 @@
+package tasks;
+
+public class Deadline extends Task {
+
+    private final String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + by + ")";
+    }
+}
