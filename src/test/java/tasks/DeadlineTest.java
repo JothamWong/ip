bff1cd10b1d13c357390a70e1ac1b5c46230ecf1
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-08-18 18:32:04 +0800

    test: Add negative input when parsing Deadline

--- src/test/java/tasks/DeadlineTest.java
+++ src/test/java/tasks/DeadlineTest.java
@@ -1,13 +1,14 @@
 package tasks;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
 import org.junit.jupiter.api.Test;
 
 import misc.PepeException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DeadlineTest {
     @Test
@@ -19,5 +20,11 @@ public class DeadlineTest {
         } catch (PepeException e) {
             assert false;
         }
+    }
+
+    @Test
+    public void testDeadlineFailInput() {
+        String userInput = "meet Pepe /by 2025-08-16 140";
+        assertThrows(DateTimeParseException.class, () -> Deadline.fromInput(userInput.split(" ")));
     }
 }
