b1f4a291fb874b9b1ccfea7dd3ecb9e06e1da973
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-08-20 18:51:17 +0800

    feat: Add isEmpty and default constructor to TaskList

--- src/main/java/state/TaskList.java
+++ src/main/java/state/TaskList.java
@@ -1,5 +1,6 @@
 package state;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,12 +12,30 @@ import tasks.Task;
 public class TaskList implements Iterable<Task> {
     private final List<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    /**
+     * Constructs a TaskList with the provided list of tasks.
+     */
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
 
     public int size() {
         return tasks.size();
+    }
+
+    /**
+     * Predicate method to check if TaskList is empty.
+     * @return true if TaskList is empty
+     */
+    public boolean isEmpty() {
+        return tasks.isEmpty();
     }
 
     public void add(Task task) {
