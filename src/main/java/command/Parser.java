3cedee56845c6bd6d8ba89e51e50a9125e8b67e2
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-08-20 22:10:15 +0800

    refactor: Use enums for command type

--- src/main/java/command/Parser.java
+++ src/main/java/command/Parser.java
@@ -12,6 +12,18 @@ import tasks.Todo;
  * Class in charge of parsing all user inputs.
  */
 public class Parser {
+
+    private enum CommandType {
+        LIST,
+        BYE,
+        MARK,
+        UNMARK,
+        EVENT,
+        TODO,
+        DEADLINE,
+        DELETE,
+        FIND,
+    }
     /**
      * Parses a user-input into a command.
      * @param input The user-input
@@ -23,23 +35,36 @@ public class Parser {
             throw new PepeException("Empty command");
         }
         String[] tokens = input.split(" ");
-        String command = tokens[0];
+        CommandType commandType = getCommandType(tokens[0]);
         String[] arguments = Arrays.copyOfRange(tokens, 1, tokens.length);
         try {
-            return switch(command) {
-            case "list" -> new DisplayListCommand();
-            case "bye" -> new ByeCommand();
-            case "mark" -> MarkCommand.fromInput(arguments);
-            case "unmark" -> UnmarkCommand.fromInput(arguments);
-            case "event" -> new AddToListCommand(Event.fromInput(arguments));
-            case "todo" -> new AddToListCommand(Todo.fromInput(arguments));
-            case "deadline" -> new AddToListCommand(Deadline.fromInput(arguments));
-            case "delete" -> DeleteCommand.fromInput(arguments);
-            case "find" -> FindCommand.fromInput(arguments);
-            default -> throw new PepeException("Unknown command: " + command);
+            return switch(commandType) {
+            case LIST -> new DisplayListCommand();
+            case BYE -> new ByeCommand();
+            case MARK -> MarkCommand.fromInput(arguments);
+            case UNMARK -> UnmarkCommand.fromInput(arguments);
+            case EVENT -> new AddToListCommand(Event.fromInput(arguments));
+            case TODO -> new AddToListCommand(Todo.fromInput(arguments));
+            case DEADLINE -> new AddToListCommand(Deadline.fromInput(arguments));
+            case DELETE -> DeleteCommand.fromInput(arguments);
+            case FIND -> FindCommand.fromInput(arguments);
             };
         } catch (DateTimeParseException e) {
             throw new PepeException("Error parsing datetime: " + e.getMessage());
         }
     }
+
+    private static CommandType getCommandType(String input) throws PepeException {
+        // I really don't think this is good!
+        // I am just doing this for the tag for A-Enums.
+        if (input == null || input.isEmpty()) {
+            throw new PepeException("Empty command");
+        }
+        try {
+            return CommandType.valueOf(input.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new PepeException("Unknown command: " + input);
+        }
+    }
+
 }
