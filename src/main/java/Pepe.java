3fbb514dfaaa16ee7ca553397792d5d3d20c27d5
JothamWong <45916998+JothamWong@users.noreply.github.com>
2025-08-26 23:25:23 +0800

    feat: Add javafx starter code

--- src/main/java/Pepe.java
+++ src/main/java/Pepe.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 import command.Command;
 import command.Parser;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import misc.PepeException;
 import state.Storage;
 import state.TaskList;
@@ -10,12 +14,21 @@ import state.Ui;
 /**
  * The main Pepe application class.
  */
-public class Pepe {
+public class Pepe extends Application {
+    private static final String DEFAULT_FILE_PATH = "./data/pepe.txt";
+
     private static Pepe pepe;
 
     private final Ui ui;
     private final TaskList taskList;
     private final Storage storage;
+
+    /**
+     * Default constructor to construct an instance of the Pepe application from a file that contains the task list.
+     */
+    public Pepe() {
+        this(DEFAULT_FILE_PATH);
+    }
 
     /**
      * Public constructor to construct an instance of the Pepe application from a file that contains the task list.
@@ -45,9 +58,12 @@ public class Pepe {
         }
     }
 
-    public static void main(String[] args) {
-        // Hardcoded path. Will change if receive complaints
-        pepe = new Pepe("./data/pepe.txt");
-        pepe.run();
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World");
+        Scene scene = new Scene(helloWorld);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
